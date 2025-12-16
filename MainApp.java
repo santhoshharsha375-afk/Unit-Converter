3782d7b7786fc41a4074f1c2661653ccb0817f50
santhoshharsha375-afk <santhoshharsha375@gmail.com>
2025-12-16 09:56:54 +0530

    Create MainApp.java

---
+++ MainApp.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("UNIT CONVERTER & DIMENSION CHECKER");
+        System.out.println("--------------------------------------");
+
+        System.out.println("Enter value:");
+        double value = sc.nextDouble();
+
+        System.out.println("Enter from unit:");
+        String fromUnit = sc.next();
+
+        System.out.println("Enter to unit:");
+        String toUnit = sc.next();
+
+        if (DimensionChecker.isSameDimension(fromUnit, toUnit)) {
+
+            double result = value;
+
+            if (fromUnit.equals("meter") && toUnit.equals("kilometer"))
+                result = UnitConverter.meterToKilometer(value);
+
+            else if (fromUnit.equals("kilometer") && toUnit.equals("meter"))
+                result = UnitConverter.kilometerToMeter(value);
+
+            else if (fromUnit.equals("gram") && toUnit.equals("kilogram"))
+                result = UnitConverter.gramToKilogram(value);
+
+            else if (fromUnit.equals("kilogram") && toUnit.equals("gram"))
+                result = UnitConverter.kilogramToGram(value);
+
+            else if (fromUnit.equals("second") && toUnit.equals("minute"))
+                result = UnitConverter.secondsToMinutes(value);
+
+            else if (fromUnit.equals("minute") && toUnit.equals("second"))
+                result = UnitConverter.minutesToSeconds(value);
+
+            System.out.println("Converted Value: " + result);
+
+        } else {
+            System.out.println("ERROR: Units are NOT dimensionally consistent");
+        }
+
+        sc.close();
+    }
+}
